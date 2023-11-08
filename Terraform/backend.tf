00aef37c8ec6265adc5650ba774e7f9a7ca16df9
Famacil <92828760+Famacil@users.noreply.github.com>
2023-11-08 11:40:05 -0300

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "cloudnloud-app"
-    region = "us-east-1"
+    bucket = "access-logs-alb-s3-cls301023"
+region = "us-east-1"
     key = "my-key/terraform.tfstate"
   }
 }
