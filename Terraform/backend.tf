a37326f267fbbba15a336db8325219c227a50038
Cilso Carvalho <92828760+cilsocarvalho@users.noreply.github.com>
2024-08-02 21:37:48 -0300

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "access-logs-alb-s3-cls301023"
-region = "us-east-1"
-    key = "my-key/terraform.tfstate"
+  bucket = "access-logs-alb-s3-cls301023"
+  region = "us-east-1"
+    key = "my-key/"
   }
 }
